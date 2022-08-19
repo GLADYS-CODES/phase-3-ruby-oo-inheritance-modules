dd11c17128be79816ec0170828f3c8588fae2b98
GLADYS <mungaiglado106@gmail.com>
2022-08-19 15:59:47 +0300

    initial commit

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,24 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+
+  # with namespaced modules
+  # extend FancyDance::ClassMethods
+  # include FancyDance::InstanceMethods
+  
+
+  #without non namespaced modules
+
+include Dance
+extend MetaDancing
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    end
+    
+end
